6eb3cae435b046ad67b5ee619934995e4062ff30
lacie-life <00sao00ios00@gmail.com>
2021-12-26 20:28:02 +0700

    [Update]

--- OpenCVInstall.sh
+++ OpenCVInstall.sh
@@ -8,41 +8,41 @@ echo "Dependencies Install"
 
 sudo apt upgrade
 
-sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall
+sudo apt install -y build-essential cmake pkg-config unzip yasm git checkinstall
 
-sudo apt install libjpeg-dev libpng-dev libtiff-dev
+sudo apt install -y libjpeg-dev libpng-dev libtiff-dev
 
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
 
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
-sudo apt install libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt install -y libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev
 
-sudo apt install libfaac-dev libmp3lame-dev libvorbis-dev
+sudo apt install -y libfaac-dev libmp3lame-dev libvorbis-dev
 
-sudo apt install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt install -y libopencore-amrnb-dev libopencore-amrwb-dev
 
-sudo apt-get install libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
+sudo apt-get install -y libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
 
-sudo apt-get install libgtk-3-dev
+sudo apt-get install -y libgtk-3-dev
 
-sudo apt-get install qt5-default
+sudo apt-get install -y qt5-default
 
-sudo apt-get install python3-dev python3-pip
+sudo apt-get install -y python3-dev python3-pip
 
 sudo -H pip3 install -U pip numpy
 
-sudo apt install python3-testresources
+sudo apt install -y python3-testresources
 
-sudo apt-get install libtbb-dev
+sudo apt-get install -y libtbb-dev
 
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get install -y libatlas-base-dev gfortran
 
-sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libprotobuf-dev protobuf-compiler
 
-sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
 
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get install -y libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
 
 echo "OpenCV 4.2 download"
 
