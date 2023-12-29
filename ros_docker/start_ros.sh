273db4d2a0b78e5e5636e4d650bcd62c01087b41
lacie-life <00sao00ios00@gmail.com>
2023-12-29 16:47:29 +0900

    [Update]

---
+++ ros_docker/start_ros.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+xhost +
+
+sudo docker run -it --net=host \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    ros_noetic \
+    bash
+
